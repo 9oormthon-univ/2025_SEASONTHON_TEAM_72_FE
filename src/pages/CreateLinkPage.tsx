64b67d5d5229b1e40ce554ee6e92918d0659be9b
Chaeyeong Lee <pop98149814@gmail.com>
2025-09-01 21:27:33 +0900

    init: 폴더 구조 및 초기 환경 세팅

---
+++ src/pages/CreateLinkPage.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const CreateLinkPage = () => {
+  return (
+    <CreateLinkPageLayout>
+      <span>this is create link page.</span>
+    </CreateLinkPageLayout>
+  );
+};
+
+export default CreateLinkPage;
+
+export const CreateLinkPageLayout = styled.div``;
---
+++ src/routes/Router.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import CreateLinkPage from "../pages/CreateLinkPage";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* <Route element={<Layout />}> */}
+        <Route path="/createlink" element={<CreateLinkPage />} />
+        {/* </Route> */}
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
+
