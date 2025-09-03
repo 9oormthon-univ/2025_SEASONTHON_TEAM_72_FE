d04bd7eed340c46bfacdce99954adada449fa07d
yeons7j <yeons7j@gmail.com>
2025-09-04 00:11:45 +0900

    chore: 파일 구조 수정 #4

--- src/pages/SettlementStartPage.tsx
+++
@@ -1,7 +0,0 @@
-import SettlementStartContent from "../components/SettlementStart/SettlementStartContent";
-
-const SettlementStartPage = () => {
-  return <SettlementStartContent />;
-};
-
-export default SettlementStartPage;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CreateLinkPage from "../pages/CreateLinkPage";
 import Layout from "../components/Layout/Layout";
+import StartSettlementPage from "../pages/StartSettlementPage";
+
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/createlink" element={<CreateLinkPage />} />
+          <Route path="/startsettlement" element={<StartSettlementPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
