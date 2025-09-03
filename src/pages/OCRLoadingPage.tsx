4f8fc7f6f5ba2b83255ba57e6dfd53b3650705af
yeons7j <yeons7j@gmail.com>
2025-09-03 22:30:06 +0900

    feat: 로딩 페이지 생성 #11

---
+++ src/pages/OCRLoadingPage.tsx
@@ -0,0 +1,7 @@
+import OCRLoadingContent from "../components/OCRLoading/OCRLoadingContent";
+
+const OCRLoadingPage = () => {
+    return <OCRLoadingContent />;
+};
+
+export default OCRLoadingPage;
