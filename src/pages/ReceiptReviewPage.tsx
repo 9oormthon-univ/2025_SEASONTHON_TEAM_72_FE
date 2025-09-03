1349c43bcfd77e62b036546c7ffefc98f5ab16a1
yeons7j <yeons7j@gmail.com>
2025-09-04 00:15:51 +0900

    chore: 파일 구조 수정 #2

--- src/pages/ReceiptReviewPage.tsx
+++
@@ -1,7 +0,0 @@
-import ReceiptReviewContent from "../components/ReceiptReview/ReceiptReviewContent";
-
-const ReceiptReviewPage = () => {
-  return <ReceiptReviewContent />;
-};
-
-export default ReceiptReviewPage;
