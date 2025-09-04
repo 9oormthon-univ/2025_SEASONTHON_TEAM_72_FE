9ff886686d6c29c9a394327ce7a3c344619fca74
yeons7j <yeons7j@gmail.com>
2025-09-04 13:33:05 +0900

    index, router 수정

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CreateLinkPage from "../pages/CreateLinkPage";
+import ReviewReceiptPage from "../pages/ReviewReceiptPage";
+import FinishSettleupPage from "../pages/FinishSettleupPage";
 import Layout from "../components/Layout/Layout";
+
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
           <Route path="/createlink" element={<CreateLinkPage />} />
+          <Route path="/reviewreceipt" element={<ReviewReceiptPage />} />
+          <Route path="/finishsettleup" element={<FinishSettleupPage/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
