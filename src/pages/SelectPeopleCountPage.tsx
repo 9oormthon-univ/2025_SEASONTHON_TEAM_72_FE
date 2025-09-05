96ee852333ae6592689e3c214ff0890693bcc436
yeons7j <yeons7j@gmail.com>
2025-09-05 22:52:22 +0900

    feat: 정산 인원 선택 페이지 생성 #24

---
+++ src/pages/SelectPeopleCountPage.tsx
@@ -0,0 +1,7 @@
+import SelectPeopleCount from "../components/SelectPeopleCount/SelectPeopleCount";
+
+const SelectPeopleCountPage = () => {
+  return <SelectPeopleCount />;
+};
+
+export default SelectPeopleCountPage;
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ReviewReceiptPage from "../pages/ReviewReceiptPage";
 import FinishSettleupPage from "../pages/FinishSettleupPage";
 import StartSettlementPage from "../pages/StartSettlementPage";
+import SelectPeopleCountPage from "../pages/SelectPeopleCountPage";
+
 import Layout from "../components/Layout/Layout";
 
 const Router = () => {
@@ -12,6 +14,7 @@ const Router = () => {
           <Route path="/startsettlement" element={<StartSettlementPage />} />
           <Route path="/reviewreceipt" element={<ReviewReceiptPage />} />
           <Route path="/finishsettleup" element={<FinishSettleupPage />} />
+          <Route path="/selectpeoplecount" element={<SelectPeopleCountPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
