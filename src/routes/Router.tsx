aac48bff0817a8ef8e536e0d1cc0286b763913b6
yeons7j <yeons7j@gmail.com>
2025-09-05 13:43:38 +0900

    fix: 충돌 해결 #2

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,26 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
 import Layout from "../components/Layout/Layout";
+
 import ReceiptConfirmPage from "../pages/ReceiptConfirmPage";
 import ReviewReceiptPage from "../pages/ReviewReceiptPage";
 import FinishSettleupPage from "../pages/FinishSettleupPage";
-=======
-import CreateLinkPage from "../pages/CreateLinkPage";
-import ReviewReceiptPage from "../pages/ReviewReceiptPage";
-import FinishSettleupPage from "../pages/FinishSettleupPage";
-import Layout from "../components/Layout/Layout";
->>>>>>> origin/main
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-<<<<<<< HEAD
           <Route path="/receiptconfirm" element={<ReceiptConfirmPage />} />
-=======
-          <Route path="/createlink" element={<CreateLinkPage />} />
->>>>>>> origin/main
           <Route path="/reviewreceipt" element={<ReviewReceiptPage />} />
           <Route path="/finishsettleup" element={<FinishSettleupPage/>}/>
         </Route>
